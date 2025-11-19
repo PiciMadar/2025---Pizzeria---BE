9bdc189e712a81f8f9ae72c57790825c2719f985
R.Dávid <98745267+PiciMadar@users.noreply.github.com>
2025-11-19 12:12:15 +0100

    Nem commitol föl

--- modules/tables.js
+++ modules/tables.js
@@ -9,7 +9,21 @@ const upload = multer({ dest: 'uploads/' })
 const path = require('path')
 const fs = require('fs');
 const { error } = require('console');
- 
+const nodemailer = require('nodemailer')
+const ejs = require('ejs')
+
+
+var transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    }
+});
+
+
+
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -78,6 +92,30 @@ router.get('/:table/:field/:op/:value', (req, res) => {
     }, req);
  
 });
+
+
+//SEND EMAIL
+router.post('/sendmail',async (req,res)=>{
+    const {to,subject,template, data} = req.body;
+    if(!to || !subject || !template){
+        return res.status(400).send({error: "Hiányzó adatok!"})
+    }
+    try{
+        await transporter.sendMail({
+            from: process.env.ADMINMAIL,
+            to: to,
+            subject: subject,
+            html: await renderTemplate(template,data || {})
+        })
+
+        return res.status(200).send({message: 'Az email küldése sikeres!'});
+    }
+    catch(err)
+    {
+        console.log(err)
+        return res.status(500).send({error: "Hiba az email küldéssel! "})
+    }
+})
 
 //FILE UPLOAD
 router.post('/upload', upload.single('image'),(req,res) => {
@@ -244,4 +282,9 @@ function getOP(op){
     return op;
 }
 
+
+async function renderTemplate(templateName, data){
+    const tmpfile = path.join(__dirname,"..","templates",templateName + ".ejs")
+    return await ejs.renderFile(tmpfile,data)
+}
 module.exports = router;
